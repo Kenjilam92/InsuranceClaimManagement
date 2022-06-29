d0f4ae18db853106edd47a14e034f07aec94e90b
kenjilam92 <kenji.lam.92@gmail.com>
2022-06-28 21:01:39 -0400

    first commit

---
+++ src/main/java/com/hexaware/InsuranceClaimManagement/InsuranceClaimManagementApplication.java
@@ -0,0 +1,13 @@
+package com.hexaware.InsuranceClaimManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceClaimManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceClaimManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hexaware/InsuranceClaimManagement/InsuranceClaimManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexaware.InsuranceClaimManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceClaimManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
